43e69cfa6157f32a82acfd4cf23de20566e3654f
Sarah Rehan <sarahahmad17@gmail.com>
2023-03-26 13:00:24 -0400

    added winner logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,9 +7,14 @@ export const Board = () => {
     const [values, setValues] = useState([]);
     const [toggle, setToggle] = useState(false);
     const [xIsNext, setXIsNext] = useState(true);
+    let alert;
 
     const onSquareClick = (i) => {
-        if (squares[i]) {
+        if (squares[i] || calculateWinner(squares)) {
+            if (squares[i]) {
+                alert = "You cant make that move"
+                console.log("cant make move")
+            }
             return
         };
         const nextSquares = squares.slice()
@@ -23,8 +28,41 @@ export const Board = () => {
         setXIsNext(!xIsNext);
     }
 
+    function calculateWinner(squares) {
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+          return null;
+    }
+
+    const winner = calculateWinner(squares);
+    let status;
+
+    if (winner) {
+        status = "Winner: " + winner;
+    } 
+    else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+
   return (
     <>
+        <div className="alert">{alert}</div>
+        <div className="status">{status}</div>
         <div style={{ display: "flex" }}>
             <Square value={squares[0]} onSquareClick={() => onSquareClick(0)} toggle={toggle}/>
             <Square value={squares[1]} onSquareClick={() => onSquareClick(1)} toggle={toggle}/>
